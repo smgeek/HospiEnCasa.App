d29cd69dc37bebb76133206ac7fb3f3afdabd0f1
smgeek <s.melendez.c@gmail.com>
2022-08-24 09:41:16 -0500

    comentarios agregados

--- HospiEnCasa.App.Dominio/Entidades/Enfermera.cs
+++ HospiEnCasa.App.Dominio/Entidades/Enfermera.cs
@@ -1,6 +1,8 @@
 using System;
 namespace HospiEnCasa.App.Dominio{
+    //Esta es una entidad
     public class Enfermera{
+        
         public int Id {get;set;}
         public string TarjetaProfesional {get;set;}
         public int HorasLaboradas {get;set;}
